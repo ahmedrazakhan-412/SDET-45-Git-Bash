6c7b20d198be57b0af111b20f511a0d39676670e
ahmedrazakhan-412 <ahmedrazakhan412@gmail.com>
2023-01-24 15:14:38 +0530

    third commit

---
+++ src/test/java/github_package/GithubScript1_Test.java
@@ -0,0 +1,14 @@
+package github_package;
+
+import org.testng.annotations.Test;
+
+public class GithubScript1_Test {
+	
+	@Test
+	
+	public void Script1test()
+	{
+		System.out.println("--- GithubScript--1 --");
+	}
+
+}
