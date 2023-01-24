8f1d8b005d5783a310389495c72158a4ee8f523d
ahmedrazakhan-412 <ahmedrazakhan412@gmail.com>
2023-01-24 15:02:59 +0530

    second commit

---
+++ src/test/java/github_package/UpdateTest.java
@@ -0,0 +1,16 @@
+package github_package;
+
+import org.testng.annotations.Test;
+
+public class UpdateTest {
+	
+	@Test
+	public void update(){
+		System.out.println("-- update TestScript- 3");
+	}
+	
+	@Test
+	public void delete(){
+		System.out.println("-- delete TestScript deleted --");
+	}
+}
